9c03b6bb5756539a511dd40a08482226a5acb271
mono-s-kuro <89817924+mono-s-kuro@users.noreply.github.com>
2022-05-14 10:24:41 +0900

    Initial Commit

---
+++ myapp/myappApp.swift
@@ -0,0 +1,17 @@
+//
+//  myappApp.swift
+//  myapp
+//
+//  Created by shota on 2022/05/14.
+//
+
+import SwiftUI
+
+@main
+struct myappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
